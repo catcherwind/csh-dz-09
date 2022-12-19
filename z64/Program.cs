58a90499c53b15b7ef2b882ff0def7605fe1c6f6
Ksusha <ksushalive@yandex.ru>
2022-12-19 20:59:06 +0300

    add z64

---
+++ z64/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+if (m % 2 == 0) PrintValue(m,n);
+else PrintValue(m+1,n);
+
+void PrintValue(int m, int max)
+{
+    if (m > max) return;
+    Console.Write($" {m}");
+    PrintValue(m + 2, max);
+}
+
