2326cc312b297c3e37cd0e822e968d22dc6a828a
Ksusha <ksushalive@yandex.ru>
2022-12-19 21:33:32 +0300

    add z68

---
+++ z68/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"НОД:{SearchNod(n,m)}");
+
+int SearchNod(int n, int m)
+{
+    if(m == 0)
+        return n;
+    if(n > m)
+        return SearchNod(m, n % m);
+    else
+        return SearchNod(n, m % n);
+}
