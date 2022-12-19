2aa00d0703fa4d9685b29967f6b5fa85d95ededa
Ksusha <ksushalive@yandex.ru>
2022-12-19 21:04:31 +0300

    add z66

---
+++ z66/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int sum = 0;
+SumValue(m,n);
+Console.WriteLine(sum);
+
+void SumValue(int m, int max)
+{
+    if (m > max) return;
+    sum += m;
+    SumValue(m + 1, max);
+}
+
